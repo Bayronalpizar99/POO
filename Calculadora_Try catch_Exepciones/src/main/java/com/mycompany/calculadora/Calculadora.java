eea91da2ea275539a678546b2539658c71728077
Bayronalpizar99 <125934221+Bayronalpizar99@users.noreply.github.com>
2023-05-10 15:44:22 -0600

    Add files via upload

---
+++ Calculadora_Try catch_Exepciones/src/main/java/com/mycompany/calculadora/Calculadora.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.calculadora;
+
+/**
+ *
+ * @author Bayron
+ */
+public class Calculadora {
+
+    public static void main(String[] args) {
+       
+        Ventana_Calculadora cal= new Ventana_Calculadora();
+        cal.setVisible(true);
+        cal.setTitle("Calculadora");
+    }
+}
